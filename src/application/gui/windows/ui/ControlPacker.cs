f4aa6adce8cebc72fef54136549d28dc8578a76a
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-08 13:05:47 +0000

    Add winforms helper classes

---
+++ src/application/gui/windows/ui/ControlPacker.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace Codice.Examples.GuiTesting.Windows.UI
+{
+    internal static class ControlPacker
+    {
+        internal static void AddControl(Control parent, Control child)
+        {
+            parent.Controls.Add(child);
+            if (child.Dock == DockStyle.Left || child.Dock == DockStyle.Top ||
+                child.Dock == DockStyle.Fill || child.Dock == DockStyle.None)
+            {
+                child.BringToFront();
+            }
+        }
+    }
+}
