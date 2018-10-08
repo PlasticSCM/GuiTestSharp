37d0f2ce4b65a7a0a3ba531bc4122abe21e8a025
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-08 13:05:11 +0000

    Add interface that the app's main window will implement

---
+++ src/application/lib/interfaces/IApplicationWindow.cs
@@ -0,0 +1,9 @@
+﻿namespace Codice.Examples.GuiTesting.Lib.Interfaces
+{
+    public interface IApplicationWindow
+    {
+        void AddItemToList(string item);
+        void RemoveItemFromList(string item);
+        void ClearInput();
+    }
+}
