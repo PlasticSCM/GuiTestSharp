b3f1f437822d04d2e8a509d73fc41546f7d6a3f7
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-08 13:03:27 +0000

    Add a Localization class with texts for the apps

---
+++ src/application/lib/Localization.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Codice.Examples.GuiTesting.Lib
+{
+    public class Localization
+    {
+        public enum Name
+        {
+            AddButton,
+            RemoveButton,
+            TextInputLabel
+        }
+
+        Localization()
+        {
+            mTexts = new Dictionary<Name, string>();
+            mTexts.Add(Name.AddButton, "Add");
+            mTexts.Add(Name.RemoveButton, "Remove");
+            mTexts.Add(Name.TextInputLabel, "Text: ");
+        }
+
+        public static string GetText(Name name)
+        {
+            return GetInstance().TryGetString(name);
+        }
+
+        static Localization GetInstance()
+        {
+            if (mInstance == null)
+                mInstance = new Localization();
+
+            return mInstance;
+        }
+
+        string TryGetString(Name name)
+        {
+            if (!mTexts.ContainsKey(name))
+                return "UNLOCALIZED";
+
+            return mTexts[name];
+        }
+
+        static Localization mInstance;
+
+        readonly Dictionary<Name, string> mTexts;
+    }
+}
