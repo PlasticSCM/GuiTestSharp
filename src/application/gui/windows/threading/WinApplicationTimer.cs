fb14c95beb6c3c482e45039ed70626dd87e93af6
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-09 08:58:57 +0000

    Add an adaptation of Codice's ThreadWaiter
    This provides easy long-running background operations, with the option
    of post-operation actions as well.
    The timer must be implemented on a per-platform basis, so for now I'll
    add the one that works OK with WinForms.

---
+++ src/application/gui/windows/threading/WinApplicationTimer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+using Codice.Examples.GuiTesting.Lib.Threading;
+
+namespace Codice.Examples.GuiTesting.Windows.Threading
+{
+    public class WinPlasticTimerBuilder : IApplicationTimerBuilder
+    {
+        IApplicationTimer IApplicationTimerBuilder.Get(
+            bool bModalMode, ThreadWaiter.TimerTick timerTickDelegate)
+        {
+            return new WinPlasticTimer(DEFAULT_TIMER_INTERVAL, timerTickDelegate);
+        }
+
+        const int DEFAULT_TIMER_INTERVAL = 100;
+    }
+
+    public class WinPlasticTimer : IApplicationTimer
+    {
+        public WinPlasticTimer(int timerInterval, ThreadWaiter.TimerTick timerTickDelegate)
+        {
+            mTimerInterval = timerInterval;
+            mTimerTickDelegate = timerTickDelegate;
+        }
+
+        public void Start()
+        {
+            mTimer = new Timer();
+            mTimer.Interval = mTimerInterval;
+            mTimer.Tick += OnTimerTick;
+
+            mTimer.Start();
+        }
+
+        public void Stop()
+        {
+            mTimer.Stop();
+            mTimer.Tick -= OnTimerTick;
+            mTimer.Dispose();
+        }
+
+        void OnTimerTick(object sender, EventArgs e)
+        {
+            mTimerTickDelegate();
+        }
+
+        Timer mTimer;
+
+        readonly int mTimerInterval;
+        readonly ThreadWaiter.TimerTick mTimerTickDelegate;
+    }
+}
