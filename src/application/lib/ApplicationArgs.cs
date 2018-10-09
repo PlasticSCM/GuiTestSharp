4ab4ff5c70b78b816426ba6c5b3cc9f345e57237
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-09 10:59:22 +0000

    Add a class to parse arguments passed to the application

---
+++ src/application/lib/ApplicationArgs.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace Codice.Examples.GuiTesting.Lib
+{
+    public class ApplicationArgs
+    {
+        public readonly bool IsTestingMode;
+        public readonly string PathToAssemblies;
+
+        ApplicationArgs(bool isTestingMode, string pathToAssemblies)
+        {
+            IsTestingMode = isTestingMode;
+            PathToAssemblies = pathToAssemblies;
+        }
+
+        public static ApplicationArgs Parse(string[] args)
+        {
+            if (args.Length < 2
+                || args[0] != TESTING_FLAG
+                || !Directory.Exists(args[1]))
+            {
+                return new ApplicationArgs(false, string.Empty);
+            }
+
+            return new ApplicationArgs(true, args[1]);
+        }
+
+        const string TESTING_FLAG = "--testing";
+    }
+}
