09db298318dfd9aa3ac305564e136ad93273a2a9
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-09 10:45:57 +0000

    Add the ITesteableApplicationWindow
    This interface will be implemented by all of the GUIs (Windows, Linux,
    macOS), and it goes on its own assembly because it is regarding testing,
    not application logic.
    This interface provides a uniform method of accessing and modifying
    GUI elements and its properties in a platform agnostic way.

---
+++ src/testing/guitestinterfaces/ITesteableApplicationWindow.cs
@@ -0,0 +1,17 @@
+﻿namespace Codice.Examples.GuiTesting.GuiTestInterfaces
+{
+    public interface ITesteableApplicationWindow
+    {
+        void ClickAddButton();
+        void ClickRemoveButton();
+
+        void ChangeText(string text);
+        string GetText();
+
+        int GetItemsInListCount();
+        string GetItemInListAt(int index);
+
+        string GetProgressMessage();
+        string GetErrorMessage();
+    }
+}
