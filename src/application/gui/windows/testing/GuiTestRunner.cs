c23c1651020deb4fcf2b5755daef378bfef8176e
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-16 12:59:32 +0000

    Add three helper classes regarding GUI testing
    WindowHandler is in charge of the application lifecycle, both in normal
    and testing modes.
    GuiTestRunner is self-explanatory, it is used to run GUI tests.
    TestInfoReader is a helper class to deserialize the file on the path
    added to the ApplicationArgs on the previous commit.

---
+++ src/application/gui/windows/testing/GuiTestRunner.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+using PNUnit.Framework;
+using NUnit.Util;
+
+namespace Codice.Examples.GuiTesting.Windows.Testing
+{
+    public class GuiTestRunner
+    {
+        public interface IGuiFinalizer
+        {
+            void Finalize(int exitCode);
+        }
+
+        public GuiTestRunner(
+            string testInfoFile,
+            string pathToAssemblies,
+            IGuiFinalizer guiFinalizer,
+            ITestConsoleAccess guiConsole)
+        {
+            mTestInfoFile = testInfoFile;
+            mPathToAssemblies = pathToAssemblies;
+            mGuiFinalizer = guiFinalizer;
+            mGuiConsole = guiConsole;
+        }
+
+        public void Run(object state)
+        {
+            InitServices.InitNUnitServices();
+
+            PNUnitTestInfo testInfo = TestInfoReader.ReadTestInfo(mTestInfoFile);
+
+            if (testInfo == null)
+            {
+                Console.Error.WriteLine("Cannot execute tests without information. Exiting...");
+                mGuiFinalizer.Finalize(1);
+            }
+
+            // TODO run the test, obviously :-)
+        }
+
+        static IPNUnitServices GetPNUnitServices(string server)
+        {
+            return (IPNUnitServices)Activator.GetObject(
+                typeof(IPNUnitServices),
+                string.Format("tcp://{0}/IPNUnitServices", server));
+        }
+
+        readonly string mTestInfoFile;
+        readonly string mPathToAssemblies;
+        readonly IGuiFinalizer mGuiFinalizer;
+        readonly ITestConsoleAccess mGuiConsole;
+
+        internal static class GuiTestAssemblyResolver
+        {
+            internal static void InstallAssemblyResolver(
+                string pathToAssemblies, List<string> assemblies)
+            {
+                mPathToAssemblies = pathToAssemblies;
+                mAssemblies = assemblies;
+
+                AppDomain.CurrentDomain.AssemblyResolve += Resolve;
+            }
+
+            internal static void UninstallAssemblyResolver()
+            {
+                AppDomain.CurrentDomain.AssemblyResolve -= Resolve;
+            }
+
+            static Assembly Resolve(object sender, ResolveEventArgs e)
+            {
+                string[] asm = e.Name.Split(',');
+
+                string assemblyName = asm[0];
+
+                if (!assemblyName.EndsWith(".dll"))
+                    assemblyName += ".dll";
+
+                if (mAssemblies == null || !mAssemblies.Contains(assemblyName))
+                    return null;
+
+                string assemblyFullPath = Path.Combine(mPathToAssemblies, assemblyName);
+
+                return Assembly.LoadFrom(assemblyFullPath);
+            }
+
+            static string mPathToAssemblies;
+            static List<string> mAssemblies;
+        }
+
+        class InitServices
+        {
+            internal static void InitNUnitServices()
+            {
+                ServiceManager.Services.AddService(new SettingsService());
+                ServiceManager.Services.AddService(new DomainManager());
+                ServiceManager.Services.AddService(new ProjectService());
+
+                NUnit.Core.CoreExtensions.Host.InitializeService();
+                ServiceManager.Services.InitializeServices();
+            }
+        }
+    }
+}
---
+++ src/application/gui/windows/testing/TestInfoReader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters;
+
+using log4net;
+
+using PNUnit.Framework;
+
+namespace Codice.Examples.GuiTesting.Windows.Testing
+{
+    internal class TestInfoReader
+    {
+        internal static PNUnitTestInfo ReadTestInfo(string testInfoPath)
+        {
+            int init = Environment.TickCount;
+            try
+            {
+                using (FileStream fs = new FileStream(testInfoPath, FileMode.Open, FileAccess.Read))
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    bf.AssemblyFormat = FormatterAssemblyStyle.Simple;
+                    return (PNUnitTestInfo)bf.Deserialize(fs);
+                }
+            }
+            catch (Exception ex)
+            {
+                mLog.ErrorFormat(
+                    "Something went wrong while reading the agent info: {0}",
+                    ex.Message);
+                return null;
+            }
+            finally
+            {
+                TryDelete(testInfoPath);
+                mLog.DebugFormat(
+                    "Read test info file {0} in {1} ms.",
+                    testInfoPath, Environment.TickCount - init);
+            }
+        }
+
+        static void TryDelete(string testInfoPath)
+        {
+            try
+            {
+                File.Delete(testInfoPath);
+            }
+            catch (Exception ex)
+            {
+                mLog.WarnFormat(
+                    "Could not delete test info file {0}: {1}",
+                    testInfoPath, ex.Message);
+
+                mLog.DebugFormat(
+                    "StackTrace:{0}{1}",
+                    Environment.NewLine, ex.StackTrace);
+            }
+        }
+
+        static readonly ILog mLog = LogManager.GetLogger("TestInfoReader");
+    }
+}
