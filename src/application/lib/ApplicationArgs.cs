cd806ac2b850a0286d93727f41bf2e634ac60f39
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-16 12:56:19 +0000

    Add the TestInfoFile member to ApplicationArgs
    This will be the path of a temporal file containing information
    regarding the test(s) to run.

--- src/application/lib/ApplicationArgs.cs
+++ src/application/lib/ApplicationArgs.cs
@@ -6,23 +6,29 @@ namespace Codice.Examples.GuiTesting.Lib
     {
         public readonly bool IsTestingMode;
         public readonly string PathToAssemblies;
+        public readonly string TestInfoFile;
 
-        ApplicationArgs(bool isTestingMode, string pathToAssemblies)
+        ApplicationArgs(
+            bool isTestingMode,
+            string pathToAssemblies,
+            string testInfoFile)
         {
             IsTestingMode = isTestingMode;
             PathToAssemblies = pathToAssemblies;
+            TestInfoFile = testInfoFile;
         }
 
         public static ApplicationArgs Parse(string[] args)
         {
-            if (args.Length < 2
+            if (args.Length < 3
                 || args[0] != TESTING_FLAG
-                || !Directory.Exists(args[1]))
+                || !Directory.Exists(args[1])
+                || !File.Exists(args[2]))
             {
-                return new ApplicationArgs(false, string.Empty);
+                new ApplicationArgs(false, string.Empty, string.Empty);
             }
 
-            return new ApplicationArgs(true, args[1]);
+            return new ApplicationArgs(true, args[1], args[2]);
         }
 
         const string TESTING_FLAG = "--testing";
