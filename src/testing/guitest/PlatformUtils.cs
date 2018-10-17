14cf280622e9508fc225dfdff0a3cf8f40fcddae
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-17 11:47:00 +0000

    Add PlatformUtils class
    It will be useful to check the OS in which the test is running.
    We could've use the OSVersionName for that, but this is way easier to
    check.

---
+++ src/testing/guitest/PlatformUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace GuiTest
+{
+    public static class PlatformUtils
+    {
+        public enum Platform
+        {
+            Unknown,
+            Windows,
+            MacOS,
+            Linux
+        }
+
+        public static Platform CurrentPlatform
+        {
+            get
+            {
+                if (mCurrentPlatform != Platform.Unknown)
+                    return mCurrentPlatform;
+
+                mCurrentPlatform = GetCurrentPlatform();
+                return mCurrentPlatform;
+            }
+        }
+
+        static Platform GetCurrentPlatform()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.MacOSX:
+                    return Platform.MacOS;
+
+                case PlatformID.Unix:
+                    return AreMacOSDirectoriesPresent()
+                        ? Platform.MacOS
+                        : Platform.Linux;
+
+                default:
+                    return Platform.Windows;
+            }
+        }
+
+        static bool AreMacOSDirectoriesPresent()
+        {
+            return Directory.Exists("/Applications")
+                && Directory.Exists("/System")
+                && Directory.Exists("/Users")
+                && Directory.Exists("/Volumes");
+        }
+
+        static Platform mCurrentPlatform = Platform.Unknown;
+    }
+}
