eee4b2fab07668eda75197995367891870d3dbe8
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-18 15:40:27 +0000

    Change order of args in ApplicationArgs
    The correct arguments and their order while on testing are:
      --testing <test info file> <path to assemblies>
    
    The 'path to assemblies' refers to those assemblies necessary for the
    testing:
    * guitest.dll
    * guitestinterfaces.dll
    * nunit.core.dll
    * nunit.core.interfaces.dll
    * nunit.util.dll
    * pnunit.framework.dll
    
    All of those assemblies are not required to run the application outside
    of testing, so they are not copied to the bin output directorio, in order
    to reduce clutter.

--- src/application/lib/ApplicationArgs.cs
+++ src/application/lib/ApplicationArgs.cs
@@ -5,25 +5,25 @@ namespace Codice.Examples.GuiTesting.Lib
     public class ApplicationArgs
     {
         public readonly bool IsTestingMode;
-        public readonly string PathToAssemblies;
         public readonly string TestInfoFile;
+        public readonly string PathToAssemblies;
 
         ApplicationArgs(
             bool isTestingMode,
-            string pathToAssemblies,
-            string testInfoFile)
+            string testInfoFile,
+            string pathToAssemblies)
         {
             IsTestingMode = isTestingMode;
-            PathToAssemblies = pathToAssemblies;
             TestInfoFile = testInfoFile;
+            PathToAssemblies = pathToAssemblies;
         }
 
         public static ApplicationArgs Parse(string[] args)
         {
             if (args.Length < 3
                 || args[0] != TESTING_FLAG
-                || !Directory.Exists(args[1])
-                || !File.Exists(args[2]))
+                || !File.Exists(args[1])
+                || !Directory.Exists(args[2]))
             {
                 return new ApplicationArgs(false, string.Empty, string.Empty);
             }
