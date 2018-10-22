e9c3bf899d0466b3bb478b6c79982a61f34abce7
Sergio Luis Para <sluisp@codicesoftware.com>
2018-10-22 23:20:59 +0000

    Add a platform-agnostic way of showing GUI messages

---
+++ src/application/lib/IGuiMessage.cs
@@ -0,0 +1,22 @@
+﻿namespace Codice.Examples.GuiTesting.Lib.Interfaces
+{
+    public class GuiMessage
+    {
+        public interface IGuiMessage
+        {
+            void ShowError(string title, string message);
+        }
+
+        public static void Initialize(IGuiMessage guiMessage)
+        {
+            mGuiMessage = guiMessage;
+        }
+
+        public static void ShowError(string title, string message)
+        {
+            mGuiMessage.ShowError(title, message);
+        }
+
+        static IGuiMessage mGuiMessage;
+    }
+}
