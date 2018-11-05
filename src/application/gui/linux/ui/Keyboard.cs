fbe864de60791d3edb6bb9d2b83851775b6ac400
Sergio Luis Para <sluisp@codicesoftware.com>
2018-11-05 11:36:07 +0000

    Add classes to handle with mouse and keyboard events

---
+++ src/application/gui/linux/ui/Keyboard.cs
@@ -0,0 +1,25 @@
+﻿using Gdk;
+
+namespace Codice.Examples.GuiTesting.Linux.UI
+{
+    internal static class Keyboard
+    {
+        internal static bool IsEnterPressed(EventKey eventKey)
+        {
+            Key key = eventKey.Key;
+
+            return key == Key.Return
+                || key == Key.ISO_Enter
+                || key == Key.Key_3270_Enter
+                || key == Key.KP_Enter;
+        }
+
+        internal static bool IsSpacePressed(EventKey eventKey)
+        {
+            Key key = eventKey.Key;
+
+            return key == Key.space
+                || key == Key.KP_Space;
+        }
+    }
+}
---
+++ src/application/gui/linux/ui/Mouse.cs
@@ -0,0 +1,14 @@
+﻿using Gdk;
+
+namespace Codice.Examples.GuiTesting.Linux.UI
+{
+    internal static class Mouse
+    {
+        internal static bool IsRightMouseButtonPressed(EventButton eventButton)
+        {
+            return eventButton.Button == RIGTH_MOUSE_BUTTON;
+        }
+
+        const int RIGTH_MOUSE_BUTTON = 3;
+    }
+}
