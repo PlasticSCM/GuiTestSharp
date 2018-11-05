65c125ca522596006339d585c5cfd02906e143c8
Sergio Luis Para <sluisp@codicesoftware.com>
2018-11-05 11:37:49 +0000

    Add some basic ListView class infrastructure

---
+++ src/application/gui/linux/ui/GtkListView.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+namespace Codice.Examples.GuiTesting.Linux.UI
+{
+    internal class GtkListView<T>
+    {
+        internal delegate Gtk.TreePath FindRowFunc(Gtk.TreeModel model, T value);
+
+        internal readonly Gtk.TreeView View;
+
+        internal GtkListView(
+            List<Gtk.TreeViewColumn> columns,
+            Dictionary<int, Gtk.TreeIterCompareFunc> sortFunctionsByColumn,
+            Gtk.TreeModelFilterVisibleFunc visibleFunc)
+        {
+            View = TreeBuilder.CreateTreeView();
+            mSortFunctionByColumn = sortFunctionsByColumn;
+            mVisibleFunc = visibleFunc;
+
+            foreach (Gtk.TreeViewColumn column in columns)
+                View.AppendColumn(column);
+        }
+
+        internal void Fill(List<T> values)
+        {
+            if (values == null)
+                return;
+
+            Gtk.ListStore listStore = new Gtk.ListStore(typeof(T));
+
+            values.ForEach((val) => listStore.AppendValues(val));
+
+            mModelFilter = new Gtk.TreeModelFilter(listStore, null);
+            mModelFilter.VisibleFunc = mVisibleFunc;
+
+            mModelSort = new Gtk.TreeModelSort(mModelFilter);
+            SetSortFunctions(mModelSort, mSortFunctionByColumn);
+
+            if (View.Model != null)
+                (View.Model as Gtk.TreeModelSort).Dispose();
+
+            View.Model = mModelSort;
+        }
+
+        internal List<T> GetSelected()
+        {
+            List<T> result = new List<T>();
+
+            if (View.Selection.CountSelectedRows() == 0)
+                return result;
+
+            foreach (Gtk.TreePath path in View.Selection.GetSelectedRows())
+                result.Add(GetObjectFromTreePath(mModelSort, path));
+
+            return result;
+        }
+
+        static T GetObjectFromTreePath(Gtk.TreeModelSort modelSort, Gtk.TreePath path)
+        {
+            Gtk.TreeIter iter;
+            modelSort.GetIter(out iter, path);
+            return GetObjectFromTreeIter(modelSort, iter);
+        }
+
+        static T GetObjectFromTreeIter(Gtk.TreeModel model, Gtk.TreeIter iter)
+        {
+            return (T)model.GetValue(iter, 0);
+        }
+
+        static void SetSortFunctions(
+            Gtk.TreeModelSort treeModelSort,
+            Dictionary<int, Gtk.TreeIterCompareFunc> sortFunctions)
+        {
+            foreach (int sortColumnId in sortFunctions.Keys)
+                treeModelSort.SetSortFunc(sortColumnId, sortFunctions[sortColumnId]);
+        }
+
+        Gtk.TreeModelFilter mModelFilter;
+        Gtk.TreeModelSort mModelSort;
+
+        readonly Dictionary<int, Gtk.TreeIterCompareFunc> mSortFunctionByColumn;
+        readonly Gtk.TreeModelFilterVisibleFunc mVisibleFunc;
+    }
+}
---
+++ src/application/gui/linux/ui/TreeBuilder.cs
@@ -0,0 +1,83 @@
+﻿using Gtk;
+
+namespace Codice.Examples.GuiTesting.Linux.UI
+{
+    internal static class TreeBuilder
+    {
+        internal static TreeView CreateTreeView()
+        {
+            TreeView result = new TreeView();
+            result.FixedHeightMode = true;
+            result.ButtonPressEvent += TreeView_ProtectButtonPressMultipleSelection;
+            result.KeyPressEvent += TreeView_ProtectActivateMultipleSelection;
+
+            return result;
+        }
+
+        internal static TreeViewColumn CreateColumn(
+            string label,
+            int fixedWidth,
+            TreeCellDataFunc renderFunc)
+        {
+            TreeViewColumn result = new TreeViewColumn()
+            {
+                Title = label,
+                Sizing = TreeViewColumnSizing.Fixed,
+                FixedWidth = fixedWidth,
+                Resizable = true,
+                Reorderable = false,
+                SortIndicator = false
+            };
+
+            CellRendererText cell = new CellRendererText();
+            result.PackStart(cell, true);
+            result.SetCellDataFunc(cell, renderFunc);
+
+            return result;
+        }
+
+        [GLib.ConnectBefore]
+        static void TreeView_ProtectButtonPressMultipleSelection(
+            object sender, ButtonPressEventArgs e)
+        {
+            TreeView treeView = sender as TreeView;
+
+            if (!Mouse.IsRightMouseButtonPressed(e.Event))
+            {
+                e.RetVal = false;
+                return;
+            }
+
+            TreePath treePath;
+            treeView.GetPathAtPos((int)e.Event.X, (int)e.Event.Y, out treePath);
+
+            foreach (TreePath selectedRow in treeView.Selection.GetSelectedRows())
+            {
+                if (selectedRow.Equals(treePath))
+                {
+                    e.RetVal = true;
+                    return;
+                }
+            }
+
+            e.RetVal = false;
+        }
+
+        [GLib.ConnectBefore]
+        static void TreeView_ProtectActivateMultipleSelection(
+            object sender, KeyPressEventArgs args)
+        {
+            TreeView treeView = sender as TreeView;
+
+            if (Keyboard.IsEnterPressed(args.Event)
+                || Keyboard.IsSpacePressed(args.Event))
+            {
+                treeView.ActivateRow(null, null);
+                args.RetVal = true;
+                return;
+            }
+
+            args.RetVal = false;
+        }
+    }
+}
