867ee987403fc96965d2a513354afe69208c8a15
Sergio Luis Para <sluisp@codicesoftware.com>
2018-11-08 14:04:24 +0100

    Complete the ProgressControls class implementation

--- src/application/gui/linux/ui/ProgressControls.cs
+++ src/application/gui/linux/ui/ProgressControls.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Gdk;
+using Gtk;
 
 using Codice.Examples.GuiTesting.Lib.Interfaces;
 
@@ -10,27 +7,77 @@ namespace Codice.Examples.GuiTesting.Linux.UI
 {
     internal class ProgressControls : IProgressControls
     {
-        bool IProgressControls.HasError
+        public bool HasError { get { return mbHasError; } }
+        public Label ProgressLabel { get { return mProgressLabel; } }
+        
+        internal ProgressControls(Label progressLabel, Widget[] widgets)
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
+            mProgressLabel = progressLabel;
+            mWidgets = widgets;
         }
 
         void IProgressControls.HideProgress()
         {
-            throw new NotImplementedException();
+            mProgressLabel.Visible = false;
+
+            EnableWidgets(mWidgets);
+
+            if (mbFocusedWidget != null)
+                mbFocusedWidget.GrabFocus();
         }
 
         void IProgressControls.ShowError(string message)
         {
-            throw new NotImplementedException();
+            mbHasError = true;
+
+            mProgressLabel.ModifyFg(StateType.Normal, COLOR_RED);
+            mProgressLabel.Text = message;
+            mProgressLabel.Visible = true;
         }
 
         void IProgressControls.ShowProgress(string message)
         {
-            throw new NotImplementedException();
+            mbHasError = false;
+
+            mProgressLabel.ModifyFg(StateType.Normal, COLOR_BLACK);
+            mProgressLabel.Text = message;
+            mProgressLabel.Visible = true;
+
+            mbFocusedWidget = GetFocusedWidget(mWidgets);
+
+            DisableWidgets(mWidgets);
         }
+
+        static void DisableWidgets(Widget[] widgets)
+        {
+            foreach (Widget widget in  widgets)
+                widget.Sensitive = false;
+        }
+
+        static void EnableWidgets(Widget[] widgets)
+        {
+            foreach (Widget widget in  widgets)
+                widget.Sensitive = true;
+        }
+
+        static Widget GetFocusedWidget(Widget[] widgets)
+        {
+            foreach (Widget widget in widgets)
+            {
+                if (widget.HasFocus)
+                    return widget;
+            }
+
+            return null;
+        }
+
+        Widget mbFocusedWidget;
+        bool mbHasError;
+
+        readonly Label mProgressLabel;
+        readonly Widget[] mWidgets;
+
+        static readonly Color COLOR_RED = new Color(255, 0, 0);
+        static readonly Color COLOR_BLACK = new Color(0, 0, 0);
     }
 }
