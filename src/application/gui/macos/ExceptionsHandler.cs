c9592e52b4478d199102885c5bc2781d07d81da2
Sergio Luis Para <sluisp@codicesoftware.com>
2018-12-07 04:14:01 -0800

    Implement mac exception handler

--- src/application/gui/macos/ExceptionsHandler.cs
+++ src/application/gui/macos/ExceptionsHandler.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
+using Codice.Examples.GuiTesting.GuiTestInterfaces;
+
 namespace Codice.Examples.GuiTesting.MacOS
 {
     internal class ExceptionsHandler
     {
         internal static void SetExceptionHandlers(bool isTestingMode)
         {
-            // TODO: implement
+            if (isTestingMode)
+            {
+                SetTestingExceptionHandlers();
+                return;
+            }
+
+            SetStandardExceptionHandlers();
+        }
+
+        static void SetStandardExceptionHandlers()
+        {
+            AppDomain.CurrentDomain.UnhandledException += HandleUndhandledException;
+        }
+
+        static void SetTestingExceptionHandlers()
+        {
+            AppDomain.CurrentDomain.UnhandledException += HandleTestingUnhandledException;
+        }
+
+        static void HandleUndhandledException(
+            object sender, UnhandledExceptionEventArgs e)
+        {
+            // Here you'd usually log the exception, send telemetry events...
+        }
+
+        static void HandleTestingUnhandledException(
+            object sender, UnhandledExceptionEventArgs e)
+        {
+            // We don't want to eat up the exception during testing like nothing
+            // happens, but we still want to save it in order to display it
+            // as the cause of the failed test.
+            GuiTesteableServices.UnhandledException = (Exception)e.ExceptionObject;
         }
     }
 }
