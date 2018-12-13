0df008cf364f32db0d81ef95f57ec7234b54d37c
Sergio Luis Para <sluisp@codicesoftware.com>
2018-12-12 17:31:48 -0800

    Complete MacOsGuiMessage implementation

--- src/application/gui/macos/MacOsGuiMessage.cs
+++ src/application/gui/macos/MacOsGuiMessage.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using AppKit;
 
+using Codice.Examples.GuiTesting.Lib;
 using Codice.Examples.GuiTesting.Lib.Interfaces;
 
 namespace Codice.Examples.GuiTesting.MacOS
@@ -8,7 +9,24 @@ namespace Codice.Examples.GuiTesting.MacOS
     {
         void GuiMessage.IGuiMessage.ShowError(string title, string message)
         {
-            throw new NotImplementedException();
+            NSAlert alert = new NSAlert();
+            alert.MessageText = title;
+            alert.InformativeText = message;
+            alert.Icon = NSImage.ImageNamed(NSImageName.Caution);
+            alert.AlertStyle = NSAlertStyle.Warning;
+
+            NSButton closeButton = alert.AddButton(
+                Localization.GetText(Localization.Name.Ok));
+            closeButton.KeyEquivalent = ESC_KEY_EQUIVALENT;
+
+            WindowHandler.SetActiveDialogForTesting(alert);
+
+            alert.RunModal();
+
+            WindowHandler.RemoveDialogForTesting(alert);
         }
+
+        internal static readonly string ESC_KEY_EQUIVALENT =
+            ((char)0x001B).ToString();
     }
 }
